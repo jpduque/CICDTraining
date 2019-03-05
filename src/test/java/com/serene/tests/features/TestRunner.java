09a0360faf9d9e9fa9f97a7a0e06b4f2f4f8c99b
Juan Duque <jduque@mcclatchyservices.com>
2019-03-04 21:03:23 -0500

    modify structure on project

--- src/test/java/com/serene/tests/features/TestRunner.java
+++ src/test/java/com/serene/tests/features/TestRunner.java
@@ -7,7 +7,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features= "src/test/resources/features/Login/Login.feature")
+@CucumberOptions(features= "src/test/resources/Steps")
 
 public class TestRunner {
 	
--- src/test/resources/Steps/GetAlbum.feature
+++ src/test/resources/Steps/GetAlbum.feature
@@ -1,7 +1,7 @@
 Feature: Login
 
  Scenario: Get the the song
-    Given I look for album with Id "1e7d8189-e94b-4e36-ab13-d3c7f78d0652"
+    Given I look for album with Id "1199777c-d56d-431f-809a-69e69f493e40"
     When The album api is called
     Then The song information must be shown
     
